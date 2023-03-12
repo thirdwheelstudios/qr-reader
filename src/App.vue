9f04c7e7602f617b3cafef945880e2955992a5be
Phil Crossland <phil@thirdwheelstudios.com>
2023-03-12 19:40:03 +0000

    Deleted cursive

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import AppFooter from './components/AppFooter.vue'
 
 <style lang="scss">
 #app {
-  font-family: 'Baloo 2', cursive;
+  font-family: 'Baloo 2', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #303030;
