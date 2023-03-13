5e579455875cdf80ec571e910973f48f8b1648dc
Phil Crossland <phil@thirdwheelstudios.com>
2023-03-13 16:47:42 +0000

    Added method to scan using camera

--- src/composables/qrCodeScanner/index.ts
+++ src/composables/qrCodeScanner/index.ts
@@ -1,13 +1,39 @@
+import { ref } from 'vue'
 import QrScanner from 'qr-scanner'
 
 export function useQrCodeScanner() {
+  const qrScanner = ref<QrScanner>()
+
   const readQrCodeFromFile = async (file: File) => {
-    const result = await QrScanner.scanImage(file)
+    const result = await QrScanner.scanImage(file, {
+      returnDetailedScanResult: true,
+    })
 
     return result
   }
 
+  const startScanning = (videoElem: HTMLVideoElement) => {
+    const scanner = new QrScanner(
+      videoElem,
+      (result) => console.log(result),
+      {
+        returnDetailedScanResult: true,
+      }
+    )
+
+    scanner.start()
+
+    qrScanner.value = scanner
+  }
+
+  const stopScanning = () => {
+    qrScanner.value?.stop()
+  }
+
   return {
     readQrCodeFromFile,
+    qrScanner,
+    startScanning,
+    stopScanning,
   }
 }
