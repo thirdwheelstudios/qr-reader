e3a2c91e35e15e8031653f3cc181c7c2f860de35
Phil Crossland <phil@thirdwheelstudios.com>
2023-03-13 20:38:08 +0000

    Wording tweaks

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>A brief history of QR codes</h2>
+  <h2>What are QR Codes?</h2>
   <ul>
     <li>
       QR code stands for quick response code and is a type of matrix barcode
       that can store various types of data
     </li>
     <li>
-      QR code was invented in 1994 by Masahiro Hara from Denso Wave, a Japanese
-      company that is a subsidiary of Toyota Motor Corporation
+      QR codes were invented in 1994 by Masahiro Hara from Denso Wave, a
+      Japanese company that is a subsidiary of Toyota Motor Corporation
     </li>
     <li>
       The purpose was to keep track of automotive parts during manufacturing and
@@ -20,7 +20,7 @@
       board
     </li>
     <li>
-      QR code became popular outside the automotive industry due to its fast
+      QR codes became popular outside the automotive industry due to its fast
       readability and greater storage capacity compared to standard barcodes
     </li>
   </ul>
