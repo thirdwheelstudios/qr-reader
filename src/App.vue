9a2238aafa1cf3e94a43dbd14f2e3bc289afba01
Phil Crossland <phil@thirdwheelstudios.com>
2023-03-13 15:23:10 +0000

    Moved buton styles into it's own scss file

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ import AppFooter from './components/AppFooter.vue'
 </template>
 
 <style lang="scss">
+@import './assets/styles/buttons';
+
 #app {
   font-family: 'Baloo 2', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,54 +29,5 @@ html {
 
 main {
   padding: 0.5em;
-}
-
-button {
-  background-color: #6a7cfd;
-  color: #fff;
-  border: 1px solid #6a7cfd;
-  padding: 0.5em;
-  border-radius: 0.3em;
-  font-size: 1.2rem;
-  box-shadow: 0 1px 5px #5e5e5e;
-  cursor: pointer;
-  transition: background-color 0.4s, color 0.4s;
-}
-
-button.gradient {
-  background: linear-gradient(90deg, #6a7cfd, #466eee, #6a7cfd);
-  background-size: 200% 200%;
-  -webkit-animation: animateGradient 2s infinite;
-  -moz-animation: animateGradient 2s infinite;
-  animation: animateGradient 2s infinite;
-}
-
-button:hover {
-  background-color: #7686fd;
-}
-
-button[disabled] {
-  background-color: #b2b5ca;
-  color: #e9e9ec;
-  border-color: #e9e9ec;
-  cursor: not-allowed;
-}
-
-button.gradient[disabled] {
-  background: linear-gradient(90deg, #b2b5ca, #8e98b7, #b2b5ca);
-  background-size: 200% 200%;
-  border-color: #b2b5ca;
-}
-
-@keyframes animateGradient {
-  0% {
-    background-position: 10% 0%;
-  }
-  50% {
-    background-position: 91% 100%;
-  }
-  100% {
-    background-position: 10% 0%;
-  }
 }
 </style>
