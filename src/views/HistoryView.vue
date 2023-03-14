c28e54ad6737eb0a6633b9e445143457fafc1cc1
Phil Crossland <phil@thirdwheelstudios.com>
2023-03-14 20:27:36 +0000

    Load history on mounted

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { ScanResult } from '../models'
+import { db } from '../persistence'
+
+const history = ref<ScanResult[]>()
+
+onMounted(async () => {
+  history.value = await db.history.toArray()
+})
+</script>
 
 <template>
-  <p>Your previous <router-link to="/">scans</router-link> will appear here</p>
+  <template v-if="history?.length"></template>
+  <p v-else>
+    Your previous <router-link to="/">scans</router-link> will appear here
+  </p>
 </template>
 
 <style scoped lang="scss">
