0be0a1a0bb824cdc27059d0caf5135dfaac6070e
Phil Crossland <phil@thirdwheelstudios.com>
2023-03-19 13:52:52 +0000

    Added copy icon

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faQrcode } from '@fortawesome/free-solid-svg-icons'
+import { faQrcode, faCopy } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 import router from './router'
 
-library.add(faQrcode)
+library.add(faQrcode, faCopy)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
