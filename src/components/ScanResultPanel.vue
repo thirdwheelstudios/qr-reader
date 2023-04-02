0ef4fa8418fadcdad44b1add48516260e96b2c03
Phil Crossland <phil@thirdwheelstudios.com>
2023-04-02 16:50:41 +0100

    Added copy button

--- src/components/ScanResultPanel.vue
+++ src/components/ScanResultPanel.vue
@@ -5,23 +5,48 @@ interface Props {
   scanResult?: ScanResult
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+
+const copyResult = () => {
+  if (props.scanResult) {
+    navigator.clipboard.writeText(props.scanResult.data)
+  }
+}
 </script>
 
 <template>
-  <div>
-    <p v-if="scanResult">{{ scanResult.data }}</p>
+  <div class="result-container">
+    <template v-if="scanResult">
+      <p>{{ scanResult.data }}</p>
+      <button
+        type="button"
+        @click="copyResult"
+        title="Copy scan result to clipboard"
+      >
+        <font-awesome-icon :icon="['fas', 'copy']" />
+      </button>
+    </template>
     <p v-else>Waiting for scan result...</p>
   </div>
 </template>
 
 <style scoped lang="scss">
-div {
+.result-container {
   margin: 2em auto;
   background-color: #f9f9fe;
   border: 1px solid #e4e5fc;
   box-shadow: 0 1px 3px #b8bce854;
   border-radius: 0.5em;
   max-width: 500px;
+  display: flex;
+
+  p {
+    flex-grow: 1;
+  }
+
+  button {
+    margin: 0.25em;
+    padding: 0.3em 0.5em;
+  }
 }
 </style>
