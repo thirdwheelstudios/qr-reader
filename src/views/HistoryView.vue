04eb3a6378d0da0dd816fc3ceeab9a3cb911b47d
Phil Crossland <phil@thirdwheelstudios.com>
2023-04-08 14:29:21 +0100

    Added scanned date and copy and delete buttons

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -2,10 +2,20 @@
 import { ref, onMounted } from 'vue'
 import { ScanResult } from '../models'
 import { db } from '../persistence'
-import { isValidUrl } from '../utils'
+import { isValidUrl, toDisplayDate } from '../utils'
 import PanelContainer from '../components/PanelContainer.vue'
+import { useClipboard } from '../composables/clipboard'
+
+const { copyToClipboard } = useClipboard()
 
 const history = ref<ScanResult[]>()
+
+const deleteScanResult = (toDelete: ScanResult) => {
+  if (!toDelete.id) return
+
+  db.history.delete(toDelete.id)
+  history.value = history.value?.filter((item) => item.id !== toDelete.id)
+}
 
 onMounted(async () => {
   history.value = await db.history.toArray()
@@ -19,14 +29,35 @@ onMounted(async () => {
     <ul>
       <li v-for="item of history" :key="item.id">
         <PanelContainer class="history-item">
-          <a
-            v-if="isValidUrl(item.data)"
-            :href="item.data"
-            :title="`Visit ${item.data}`"
-            target="_blank"
-            >{{ item.data }}</a
-          >
-          <p v-else>{{ item.data }}</p>
+          <div>
+            <a
+              v-if="isValidUrl(item.data)"
+              :href="item.data"
+              :title="`Visit ${item.data}`"
+              target="_blank"
+              >{{ item.data }}</a
+            >
+            <div v-else>{{ item.data }}</div>
+            <div class="text-muted">
+              Scanned {{ toDisplayDate(item.scannedUtcDateTime) }}
+            </div>
+          </div>
+          <div class="buttons">
+            <button
+              type="button"
+              title="Copy this item to the clipboard"
+              @click="copyToClipboard(item.data)"
+            >
+              <font-awesome-icon :icon="['fas', 'copy']" />
+            </button>
+            <button
+              type="button"
+              title="Delete this item from your history"
+              @click="deleteScanResult(item)"
+            >
+              <font-awesome-icon :icon="['fas', 'trash']" />
+            </button>
+          </div>
         </PanelContainer>
       </li>
     </ul>
@@ -39,16 +70,37 @@ onMounted(async () => {
 <style scoped lang="scss">
 ul {
   padding-left: 0;
+  display: flex;
+  flex-direction: column-reverse;
 
   li {
     list-style: none;
 
     .history-item {
-      padding: 0.5em 1em;
-      margin: 1em 0;
+      padding: 0.5em;
+      margin: 0.5em 0;
+      display: flex;
 
       > * {
         margin: 0;
+        display: inline-flex;
+        align-items: baseline;
+        flex-grow: 1;
+      }
+
+      .text-muted {
+        margin-left: 0.25em;
+        font-size: 0.75em;
+      }
+
+      .buttons {
+        display: flex;
+        justify-content: flex-end;
+
+        button {
+          margin: 0 0.25em;
+          font-size: 0.75rem;
+        }
       }
     }
   }
