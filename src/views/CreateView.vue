1447aa6fe7a020ba696b4120d1319b0245920ae2
Phil Crossland <phil@thirdwheelstudios.com>
2023-04-09 22:21:20 +0100

    Initial create view

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,1 +1,48 @@
-<template>Create QR Code</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import QRCode from 'qrcode'
+
+const dataToEncode = ref('https://google.com')
+const encodedDataUrl = ref<string>()
+const lightColor = ref('#fff')
+const darkColor = ref('#000')
+
+const onSubmit = async () => {
+  console.log('submit')
+  encodedDataUrl.value = await QRCode.toDataURL(dataToEncode.value, {
+    width: 300,
+    errorCorrectionLevel: 'H',
+    color: {
+      dark: darkColor.value,
+      light: lightColor.value,
+    },
+  })
+}
+
+onMounted(() => onSubmit())
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <input
+      type="text"
+      name="dataToEncode"
+      v-model="dataToEncode"
+      placeholder="Enter data to encode in QR Code"
+      title="Data to encode in QR Code"
+    />
+    <img :src="encodedDataUrl" :title="dataToEncode" />
+  </form>
+</template>
+
+<style scoped lang="scss">
+form {
+  text-align: center;
+
+  img {
+    display: block;
+    margin: 0 auto;
+    padding: 1em;
+  }
+}
+</style>
